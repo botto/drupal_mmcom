8911ccac9c4e5259574b30789fcb60dcc7aa5568
Martin Eskdale Moen <martinmoen@gmail.com>
2013-10-08 22:52:13 +0100

    Added WWW_GROUP var This is the user that is hosting the page, make sure the user building is part of the same group

--- build.sh
+++ build.sh
@@ -4,20 +4,25 @@ SITE_DIR="site"
 SITE_NAME="martinmoen.com"
 DRUSH_CMD="drush -y"
 SCRIPT_PWD=`pwd`
+WWW_GROUP="_www"
 
 if [ -d $SITE_DIR ]; then
   chmod -R 777 $SITE_DIR
   rm -rf $SITE_DIR
-fi
-
-$DRUSH_CMD make $SITE_NAME.make $SITE_DIR
-if [ $? -eq 0 ]; then
-  cd $SITE_DIR
-  $DRUSH_CMD si --sites-subdir=$SITE_NAME --site-name=$SITE_NAME --db-url=sqlite://sites/$SITE_NAME/files/.ht.sqlite --account-pass=test
-
   if [ $? -eq 0 ]; then
-    cd $SCRIPT_PWD
-    php generate_alias.php -d=`pwd` -n=$SITE_NAME
-    echo "Build Complete"
+    $DRUSH_CMD make $SITE_NAME.make $SITE_DIR
+
+    if [ $? -eq 0 ]; then
+      cd $SITE_DIR
+      $DRUSH_CMD si --sites-subdir=$SITE_NAME --site-name=$SITE_NAME --db-url=sqlite://sites/$SITE_NAME/files/.ht.sqlite --account-pass=test
+      chgrp -R $WWW_GROUP sites/$SITE_NAME/files
+
+      if [ $? -eq 0 ]; then
+        cd $SCRIPT_PWD
+        php generate_alias.php -d=`pwd` -n=$SITE_NAME
+
+        echo "Build Complete"
+      fi
+    fi
   fi
 fi

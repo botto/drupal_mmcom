42bc4ebc0141b65d9da63a939b038b5d70a08f4e
Martin Eskdale Moen <martinmoen@gmail.com>
2013-10-08 23:27:54 +0100

    Bug causing site to not build if target didn't exist

--- build.sh
+++ build.sh
@@ -9,21 +9,21 @@ WWW_GROUP="_www"
 if [ -d $SITE_DIR ]; then
   chmod -R 777 $SITE_DIR
   rm -rf $SITE_DIR
+fi
+if [ $? -eq 0 ]; then
+  $DRUSH_CMD make $SITE_NAME.make $SITE_DIR
+
   if [ $? -eq 0 ]; then
-    $DRUSH_CMD make $SITE_NAME.make $SITE_DIR
+    cd $SITE_DIR
+    $DRUSH_CMD si minimal --sites-subdir=$SITE_NAME --site-name=$SITE_NAME --db-url=sqlite://sites/$SITE_NAME/files/.ht.sqlite --account-pass=test
+    chgrp -R $WWW_GROUP sites/$SITE_NAME/files
+    chmod -R g+wrx sites/$SITE_NAME/files
 
     if [ $? -eq 0 ]; then
-      cd $SITE_DIR
-      $DRUSH_CMD si minimal --sites-subdir=$SITE_NAME --site-name=$SITE_NAME --db-url=sqlite://sites/$SITE_NAME/files/.ht.sqlite --account-pass=test
-      chgrp -R $WWW_GROUP sites/$SITE_NAME/files
-      chmod -R g+wrx sites/$SITE_NAME/files
+      cd $SCRIPT_PWD
+      php generate_alias.php -d=`pwd` -n=$SITE_NAME
 
-      if [ $? -eq 0 ]; then
-        cd $SCRIPT_PWD
-        php generate_alias.php -d=`pwd` -n=$SITE_NAME
-
-        echo "Build Complete"
-      fi
+      echo "Build Complete"
     fi
   fi
 fi
